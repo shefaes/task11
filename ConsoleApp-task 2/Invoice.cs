24db5409f3288e34f09a1bde22d1d25cd0bd2dcd
unknown <shafaes@code.edu.az>
2022-07-11 20:51:57 +0400

    task2

---
+++ ConsoleApp-task 2/Invoice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_task_2
+{
+    class Invoice
+    {
+        private string account;
+        private string customer;
+        private string provider;
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+
+        public Invoice(string account, string custumer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+
+
+        }
+        public double CostCalculation(bool needEdv)
+        {   double price = 1;
+            double Edv = price * 0.18;
+
+            if (Article=="laptop")
+            {
+                price = 1400;
+            }
+            else if (Article=="SD-card")
+            {
+                price = 30;
+            }
+            else
+            {
+                price = 12;
+                Article ="usb" ;
+            }
+           
+
+            if (needEdv)
+            {
+                price = price * Quantity + Edv;
+            }
+            else
+            {
+                price = price * Quantity;
+            }
+            return price;
+        } 
+    }
+}
---
+++ ConsoleApp-task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice("678904", "Alex", "Foxtrot");
+
+            invoice.Article = "laptop";
+            invoice.Quantity = 2;
+            Console.WriteLine(invoice.CostCalculation(true));
+        }
+    }
+}
